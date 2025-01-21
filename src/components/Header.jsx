03a0b8b7f1468ef7f249ed9cbf9efda35c465127
JACKZINH <152972451+JACKZINH@users.noreply.github.com>
2025-01-21 12:21:24 +0100

    Création du header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,100 @@
 import React from "react";
-import "../assets/styles/header.css"; // Fichier CSS pour les styles
+import { Link } from "react-router-dom";
+import Logo from "../assets/img/Logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import "../assets/styles/header.css";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="header-container">
+    <nav
+      className="navbar navbar-expand-md navbar-light shadow"
+      style={{ backgroundColor: "#f1f8fc" }}>
+      <div className="container-fluid">
         {/* Logo */}
-        <div className="logo">
-          <img src="/path/to/logo.svg" alt="Logo" />
-        </div>
+        <Link className="navbar-brand" to="/">
+          <img
+            src={Logo}
+            alt="Trouve ton artisan"
+            style={{ width: "180px", height: "auto" }}
+          />
+        </Link>
 
-        {/* Barre de recherche */}
-        <div className="search-bar">
-          <input type="text" placeholder="Rechercher un artisan..." />
-        </div>
+        {/* Bouton burger (Bootstrap) */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          style={{ border: "none" }}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-        {/* Menu */}
-        <nav className="navigation">
-          <ul className="nav-links">
-            <li>
-              <a href="/batiment">Bâtiment</a>
+        {/* Contenu qui se replie (menu + recherche) */}
+        <div className="collapse navbar-collapse" id="navbarContent">
+          {/* Menu de gauche */}
+          <ul className="navbar-nav mx-auto mb-2 mb-md-0 d-flex justify-content-center gap-4">
+            <li className="nav-item">
+              <Link className="nav-link text-dark" to="/batiment">
+                Bâtiment
+              </Link>
             </li>
-            <li>
-              <a href="/services">Services</a>
+            <li className="nav-item">
+              <Link className="nav-link text-dark" to="/services">
+                Services
+              </Link>
             </li>
-            <li>
-              <a href="/fabrication">Fabrication</a>
+            <li className="nav-item">
+              <Link className="nav-link text-dark" to="/fabrication">
+                Fabrication
+              </Link>
             </li>
-            <li>
-              <a href="/alimentation">Alimentation</a>
+            <li className="nav-item">
+              <Link className="nav-link text-dark" to="/alimentation">
+                Alimentation
+              </Link>
             </li>
           </ul>
-        </nav>
 
-        {/* Menu Hamburger pour mobile */}
-        <div className="hamburger-menu">
-          <button>☰</button>
+          {/* Recherche (input group) */}
+          <form className="d-flex">
+            <div
+              className="input-group"
+              style={{
+                borderBottom: "1px solid #0074c7",
+                borderRight: "1px solid #0074c7",
+              }}>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Rechercher..."
+                aria-label="Search"
+                style={{
+                  border: "none",
+                  boxShadow: "none",
+                  borderRadius: 0,
+                  backgroundColor: " #f1f8fc",
+                }}
+              />
+              <span
+                className="input-group-text"
+                style={{
+                  border: "none",
+                  background: "none",
+                }}>
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  style={{ color: "#0074c7" }}
+                />
+              </span>
+            </div>
+          </form>
         </div>
       </div>
-    </header>
+    </nav>
   );
 };
 
